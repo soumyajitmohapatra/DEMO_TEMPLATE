297e36620d05f9217774dc452c6784533d36ee98
soumyajitmohapatra <hellosoumyajit@gmail.com>
2022-07-20 16:28:34 +0530

    logout and refresh controller added to route

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import pg from "pg";
 import config from "../../config/pgConfig.js";
 import login from "../controller/login/login.js";
+import logout from "../controller/logout/logout.js";
+import refreshToken from "../controller/refreshToken/refreshToken.js";
 
 let pool = new pg.Pool(config);
 
@@ -13,5 +15,9 @@ router.get("/testing", (req, res) =>
 );
 
 router.post("/login", (req, res) => login.handleLogin(req, res, pool));
+router.get("/refresh", (req, res) =>
+  refreshToken.handleRefreshToken(req, res, pool)
+);
+router.get("/logout", (req, res) => logout.handleLogout(req, res, pool));
 
 export default router;
