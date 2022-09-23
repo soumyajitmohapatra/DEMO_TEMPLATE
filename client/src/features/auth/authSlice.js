1167113d45a50d8bd87054c620a467e38efd2844
soumyajitmohapatra <hellosoumyajit@gmail.com>
2022-09-23 11:19:54 +0530

    Loading feature added

--- client/src/features/auth/authSlice.js
+++ client/src/features/auth/authSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const authSlice = createSlice({
   name: "auth",
-  initialState: { user: null, token: null },
+  initialState: { user: null, token: null, isLoading: false },
   reducers: {
     setCredentials: (state, action) => {
       const { user, accessToken } = action.payload;
@@ -13,8 +13,12 @@ const authSlice = createSlice({
     logOut: (state, action) => {
       state.user = null;
       state.token = null;
-      window.localStorage.removeItem("isLoggedIn");
-      window.location.reload();
+      state.isLoading = true;
+      setTimeout(() => {
+        window.localStorage.removeItem("isLoggedIn");
+        window.location.reload();
+        state.isLoading = false;
+      }, 1500);
     },
   },
 });
@@ -25,3 +29,4 @@ export default authSlice.reducer;
 
 export const selectCurrentUser = (state) => state.auth.user;
 export const selectCurrentToken = (state) => state.auth.token;
+export const selectLoadingState = (state) => state.auth.isLoading;
